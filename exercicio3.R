a4d9682a4b03d46d257d3ed071ce650402dc4636
Rafael Antunes <rafael.uminho@hotmail.com>
2015-05-27 20:23:51 +0100

    ....

--- exercicio3.R
+++ exercicio3.R
@@ -2,9 +2,10 @@
 
 #se necessário instalar...
 #install.packages("neuralnet")
-
+#install.packages("hydroGOF")
 #Usar:
 library("neuralnet")
+library(hydroGOF);
 
 
 #ler dataset de um ficheiro txt
@@ -12,78 +13,47 @@ library("neuralnet")
 #sep é o separador, que no caso é a virgula
 #dec é o caracter dos decimais, no caso o ponto
 #Rafa : D:\\Rafa\\Escola\\GitHub\\SRCR-Exercicio3\\exercicio3.txt
-dados <- read.csv("D:\\Rafa\\Escola\\GitHub\\SRCR-Exercicio3\\exercicio3.txt",
+#5 níveis de fadiga exercicio3.csv
+#2 níveis de fadiga exercicio3-fadigas.csv
+dataset <- read.csv("D:\\Rafa\\Escola\\GitHub\\SRCR-Exercicio3\\exercicio3.csv",
                      header=TRUE,
-                     sep=",",
+                     sep=";",
                      dec=".")
 #mostrar a "cabeça" do dataset
-#head(trainset)
+#head(dados)
+
+#extrair 800 casos do dataset para um novo datasete que será usado para treinar a rede neuronal 
+trainset <- dataset[1:600, ]
+
+## extrair as restantes entradas do dataset para um dataset que sera usado para testar a redeneuronal
+testset <- dataset[601:840, ]
+
+#medir o tempo
+tempo <- proc.time()
 
 #criar a rede
 rede<-neuralnet(FatigueLevel ~ KDTMean+MAMean+MVMean+TBCMean+DDCMean+DMSMean+AEDMean+ADMSLMean,
-                dados,
-                hidden=c(15,10),
-                threshold=0.05)
+                trainset,
+                hidden=c(30,20),
+                threshold=0.01)
 
 #mostrar a rede
 plot(rede)
 
-#criar uma matriz de teste
-teste<-data.frame(KDTMean=0.003348521404442, #Fadiga = 1
-                 MAMean=-0.033404750237223,
-                 MVMean=-0.025152220755905,
-                 TBCMean=0.16703901845170,
-                 DDCMean=-0.039538593130319,
-                 DMSMean=0.01458038388061,
-                 AEDMean=0.046126139282830,
-                 ADMSLMean=0.0121196527185)
+#mostrar o tempo
+proc.time() - tempo
 
+#imprimir o erro
+print(rede$result.matrix.error)
 
-teste[2,]<-data.frame(KDTMean=0.001747651084306, #Fadiga = 6
-                     MAMean=0.097482878651944,
-                     MVMean=0.115366961041514,
-                     TBCMean=-0.07807993361726,
-                     DDCMean=-0.064426638328348, 
-                     DMSMean=-0.01281824370211,
-                     AEDMean=-0.038354975497981,
-                     ADMSLMean=0.0522370512802)
-
-teste[3,]<-data.frame(KDTMean=0.001569498070280, #Fadiga = 3
-                     MAMean=0.038427963994892,
-                     MVMean=0.042207642284009,
-                     TBCMean=-0.08706136653531,
-                     DDCMean=0.034277394962323, 
-                     DMSMean=-0.00879804916891,
-                     AEDMean=0.052910995943371,
-                     ADMSLMean=-0.0205040507122)
-
-#teste sem saber o resultado (apontado para ser nivel 7)
-teste[4,]<-data.frame(KDTMean=0, 
-                      MAMean=0.2,
-                      MVMean=0.02,
-                      TBCMean=-0.2,
-                      DDCMean=-0.2, 
-                      DMSMean=-0.2,
-                      AEDMean=0.2,
-                      ADMSLMean=0.2)
-
+## definir variaveis de input para teste   
+temp_test <- subset(testset,select = c("KDTMean","MAMean","MVMean","TBCMean","DDCMean","DMSMean","AEDMean","ADMSLMean"))
 
 #correr os  testes na rede
-rede.results<-compute(rede,teste)
+rede.results<-compute(rede,temp_test)
 
-#imprimir respostas
-print(round(rede.results$net.result))
-print(rede.results$net.result)
+#imprimir resultados
+results <- data.frame(actual = testset$FatigueLevel, prediction = rede.results$net.result)
 
-
-
-
-#If needed
-#teste[3,]<-data.frame(KDTMean=, #Fadiga = 3
- #                     MAMean=,
-  #                    MVMean=,
-   #                   TBCMean=,
-    #                  DDCMean=, 
-     #                 DMSMean=,
-      #                AEDMean=,
-       #               ADMSLMean=)
+#calcular o RMSE
+rmse(c(testset$FatigueLevel),c(results$prediction))
